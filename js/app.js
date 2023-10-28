9c5b005a1726784b98dd3a77813d0ad4279728b7
Errol Vidad <errolvidad@gmail.com>
2023-10-28 01:11:04 +0000

    solved question3

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -33,7 +33,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -49,12 +49,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sum(a,b) {
+  return a+b;
+}
 
+function multiply(a,b) {
+  return a*b;
+}
+
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let answerSum = sum(sum(a, b), c);
+  let answerMultiply = multiply(multiply(a, b), c);
+  return [answerSum, answerMultiply, `${a} and ${b} and ${c} sum to ${answerSum}.`, `The product of ${a} and ${b} and ${c} is ${answerMultiply}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
